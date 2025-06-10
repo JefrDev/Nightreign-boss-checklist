c2a78a853b8bea41d8417122731631e5623e920d
JefrDev <94841564+JefrDev@users.noreply.github.com>
2025-06-10 16:58:13 +0200

    grid now works

--- src/components/BossGrid.tsx
+++ src/components/BossGrid.tsx
@@ -1,8 +1,14 @@
 import BossCard from "./BossCard";
+import "./BossGrid.css";
 
 function BossGrid() {
   return (
     <div className="grid">
+      <BossCard bossName="libra"></BossCard>
+      <BossCard bossName="libra"></BossCard>
+      <BossCard bossName="libra"></BossCard>
+      <BossCard bossName="libra"></BossCard>
+      <BossCard bossName="libra"></BossCard>
       <BossCard bossName="libra"></BossCard>
     </div>
   );
