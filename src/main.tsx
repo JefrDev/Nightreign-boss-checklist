5d8939421103697c6b80acb3622f1a72d376cdc2
JefrDev <94841564+JefrDev@users.noreply.github.com>
2025-06-13 12:19:46 +0200

    added react router for future pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import ChecklistPage from "./ChecklistPage.tsx";
 
+const router = createBrowserRouter([{
+    path: "/",
+    element: <ChecklistPage/>,
+},
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChecklistPage />
+    <RouterProvider router={router}/>
   </StrictMode>
 );
